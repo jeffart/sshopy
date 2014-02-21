3e0057472a31a150b10d8c62b66544ccbb4d66cf
jeffart <dekogha@gmail.com>
2014-02-21 18:46:56 +0100

    5- creation des regle de validation pour la saisie des categorie

--- app/Model/Category.php
+++ app/Model/Category.php
@@ -8,4 +8,12 @@
 
 class Category extends AppModel {
 
+    // regele de validation pour l'editon et la creation des categories
+
+    public $validate = array(
+        'name' => array('rule' => 'notEmpty',
+        'message' => 'Votre nom de la categorie n\'est pas valide' )  // le champ name du formulaire doit etre rempli
+    );
+
+
 } 
